95bcdd011981d698e150546a765308eb06d9e337
Kirill1018 <115730059+Kirill1018@users.noreply.github.com>
2025-01-09 06:57:58 +0300

    Add files via upload

--- ProcessingErrorsViaConveyorBelt/Program.cs
+++ ProcessingErrorsViaConveyorBelt/Program.cs
@@ -2,7 +2,7 @@ namespace ProcessingErrorsViaConveyorBelt
 {
 	public class Program
 	{
-		public async static Task Main(string[] args)
+		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
 			Song song1 = new Song("song1", "singer1", "1999",
@@ -10,7 +10,6 @@ namespace ProcessingErrorsViaConveyorBelt
 				"song2 text"), song3 = new Song("song3", "singer3", "2005",
 				"song3 text");//создание песен
 			Song[] songs = { song1, song2, song3 };//массив песен
-			using (StreamWriter streamWriter = new StreamWriter("песни.txt", true)) foreach (Song singing in songs) await streamWriter.WriteLineAsync(singing.ToString());
 			// Add services to the container.
 			builder.Services.AddControllersWithViews();
 
@@ -26,7 +25,28 @@ namespace ProcessingErrorsViaConveyorBelt
 			app.MapControllerRoute(
 				name: "default",
 				pattern: "{controller=ProcessingErrors}/{action=Processing}/{id?}");
-			app.MapGet("/", () => songs);
+			app.MapPost("/{name}/{singer}/{relYear}/" +
+				"{text}", (string name, string singer, string relYear,
+				string text) =>
+			{
+				Song song = new Song(name, singer, relYear,
+					text);
+				List<Song> songList = songs.ToList();//список песен
+				songList.Add(song);
+				songs = songList.ToArray();
+			});
+			app.MapDelete("/{name}", (string name) =>
+			{
+				Song? song = songs.FirstOrDefault(song => song.Name == name);
+				List<Song> songList = songs.ToList();
+				songList.Remove(song!);
+				songs = songList.ToArray();
+			});
+			app.MapGet("/", async () =>
+			{
+				using (StreamWriter streamWriter = new StreamWriter("песни.txt", false)) foreach (Song singing in songs) await streamWriter.WriteLineAsync(singing.ToString());
+				return songs;
+			});
 			app.Run();
 		}
 	}
